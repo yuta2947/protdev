89ab521ab74caedca428b8c57c68c203637dd2b4
yuta2947 <prnkmt2947@gmail.com>
2025-07-28 23:30:23 +0900

    Pritter導入及び.gitattributes作成（改行コードLFに統一対応)

--- api-server/src/app/api/route.ts
+++ api-server/src/app/api/route.ts
@@ -3,58 +3,50 @@ import { GoogleGenAI } from '@google/genai';
 
 const ai = new GoogleGenAI({});
 const corsHeaders = {
-    'Access-Control-Allow-Origin': 'http://localhost:5173',
-    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type',
-  };
+  'Access-Control-Allow-Origin': 'http://localhost:5173',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+};
 
 export async function GET() {
-    return  new NextResponse(
-        JSON.stringify({ message: 'Get API Response'}),
-        {
-            status: 200,
-            headers: corsHeaders,
-        }
-    );
+  return new NextResponse(JSON.stringify({ message: 'Get API Response' }), {
+    status: 200,
+    headers: corsHeaders,
+  });
 }
 
 export async function POST(request: Request) {
-    const body = await request.json();
-    try {
-        const result = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
-            contents: "あなたについて200字で紹介して"
-        })
+  const body = await request.json();
+  try {
+    const result = await ai.models.generateContent({
+      model: 'gemini-2.5-flash',
+      contents: 'あなたについて200字で紹介して',
+    });
     const aiResponse = result.text;
-        return new NextResponse(
-            JSON.stringify({ 
-                received: {
-                    postMessage: body.text,
-                    body: 'サーバからの返答',
-                    text: aiResponse
-
-                }
-             }),
-            {
-                status: 200,
-                headers:corsHeaders,
-            }
-        );
-    } catch (error) {
-        return new NextResponse(
-            JSON.stringify({ error: 'Invalid JSON' }),
-            {
-                status: 400,
-                headers: corsHeaders,
-            }
-        );
-    }
-}
-
-
-  export async function OPTIONS() {
-    return new NextResponse(null, {
-      status: 204,
+    return new NextResponse(
+      JSON.stringify({
+        received: {
+          postMessage: body.text,
+          body: 'サーバからの返答',
+          text: aiResponse,
+        },
+      }),
+      {
+        status: 200,
+        headers: corsHeaders,
+      },
+    );
+  } catch (error) {
+    return new NextResponse(JSON.stringify({ error: 'Invalid JSON' }), {
+      status: 400,
       headers: corsHeaders,
     });
   }
+}
+
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: corsHeaders,
+  });
+}
--- フロント/src/App.tsx
+++ フロント/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import React from 'react';
 import axios from 'axios';
 
@@ -7,7 +7,6 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[currentMove];
-
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
@@ -22,7 +21,6 @@ export default function Game() {
   }
 
   const moves = history.map((squares, move) => {
-
     let description;
     if (move > 0) {
       description = 'Go to move #' + move;
@@ -39,7 +37,7 @@ export default function Game() {
   return (
     <div className="game">
       <div className="game-board">
-      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
@@ -52,40 +50,37 @@ function Board({ xIsNext, squares, onPlay }) {
   async function callAiApi() {
     try {
       const response = await axios.post('http://localhost:3000/api', {
-        text: "request",
+        text: 'request',
       });
-      console.log("AIの返答：",JSON.stringify(response.data.received))
+      console.log('AIの返答:', JSON.stringify(response.data.received));
     } catch (error) {
-      console.error("エラー:", error);
+      console.error('エラー:', error);
     }
   }
 
-  function handleClick (i) {
+  function handleClick(i) {
     const nextSquares = squares.slice();
     if (squares[i] || calculateWinner(squares)) {
       return;
     }
-   
+
     if (xIsNext) {
-      nextSquares[i] = "X";
+      nextSquares[i] = 'X';
     } else {
-      nextSquares[i] = "O";
+      nextSquares[i] = 'O';
       callAiApi();
     }
     onPlay(nextSquares);
-
   }
 
   function callAi() {
+    const APIEndPint = 'http://localhost:3000/api';
 
-    const APIEndPint = 'http://localhost:3000/api'
-  
     const handleSubmit = async () => {
       try {
-        const response =await axios.post(APIEndPint, {
-          message: 'テストメッセージ'
+        const response = await axios.post(APIEndPint, {
+          message: 'テストメッセージ',
         });
-  
       } catch (error) {
         console.error('エラー：', error);
       }
@@ -95,46 +90,45 @@ function Board({ xIsNext, squares, onPlay }) {
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
-    status = "Winner: "+ winner;
+    status = 'Winner: ' + winner;
   } else {
-    status ="Next player: "+ (xIsNext ? "X" : "O")
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
-  
+
   return (
     <>
       <div className="status">{status}</div>
       {[...Array(5)].map((_, row) => (
-    <div key={row} className="board-row">
-      {[...Array(5)].map((_, col) => {
-        const index = row * 5 + col;
-        return (
-          <Square
-            key={index}
-            value={squares[index]}
-            onSquareClick={() => handleClick(index)}
-          />
-        );
-      })}
-    </div>
-
-    ))}
+        <div key={row} className="board-row">
+          {[...Array(5)].map((_, col) => {
+            const index = row * 5 + col;
+            return (
+              <Square
+                key={index}
+                value={squares[index]}
+                onSquareClick={() => handleClick(index)}
+              />
+            );
+          })}
+        </div>
+      ))}
     </>
   );
 }
 
 function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={onSquareClick}>{value}</button>
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
   );
 }
 
-
-
-function calculateWinner (squares) {
+function calculateWinner(squares) {
   const size = 5;
   const lines: number[][] = [];
-  
-  for (let row =0; row < size; row++) {
+
+  for (let row = 0; row < size; row++) {
     lines.push([...Array(size)].map((_, col) => row * size + col));
   }
 
@@ -145,10 +139,15 @@ function calculateWinner (squares) {
   lines.push([...Array(size)].map((_, i) => i * size + i));
   lines.push([...Array(size)].map((_, i) => i * size + (size - 1 - i)));
 
-
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c, d, e] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d] && squares[a] === squares[e]) {
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c] &&
+      squares[a] === squares[d] &&
+      squares[a] === squares[e]
+    ) {
       return squares[a];
     }
   }
